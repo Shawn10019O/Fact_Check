bebdc6f1a2f92899faa4ff5da660bc14350cd1cc
Shawn10019O <zmi19ml@gmail.com>
2025-07-05 18:57:35 +0900

    pus

--- tests/test_markdown.py
+++
@@ -1,7 +0,0 @@
-from core.models import results_to_markdown, SlideResult, SuspiciousClaim
-from pytest_regressions.data_regression import DataRegressionFixture
-
-def test_markdown(data_regression: DataRegressionFixture):
-    slide = SlideResult(1, "raw", "clean", "REFUTED", "bad", [SuspiciousClaim("oops")])
-    md = results_to_markdown([slide])
-    data_regression.check(md)
